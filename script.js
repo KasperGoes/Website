12f067d0817814135137ceeecb11e0aabe6a4c9c
Kasper Goes <kaspergoes@gmail.com>
2024-10-11 16:08:46 +0200

    mobile adaptability

--- script.js
+++ script.js
@@ -1,4 +1,6 @@
-document.addEventListener('DOMContentLoaded', () => {
-    console.log('Website Loaded Successfully!');
-  });
-  
+function toggleMenu() {
+    const menu = document.querySelector(".menu-links");
+    const icon = document.querySelector(".hamburger-icon");
+    menu.classList.toggle("open");
+    icon.classList.toggle("open");
+  }
